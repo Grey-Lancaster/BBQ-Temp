185e659c19d389fd51c89a26f0962196fd2789b5
Grey Lancaster <grey@smallbizserver.com>
2023-10-09 19:57:38 -0400

    Added Max 7219 Display

--- src/moonshine3.ino
+++ src/moonshine3.ino
@@ -2,6 +2,8 @@
 #include <WebServer.h>
 #include <max6675.h>
 #include <WiFiManager.h>
+#include <MD_MAX72xx.h>
+#include <MD_Parola.h>
 // WiFi credentials
 //const char* ssid = "shop2";
 //const char* password = "mine0313";
@@ -12,6 +14,19 @@ int thermoCS = 5;
 int thermoCLK = 18;
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+
+#define CLK_PIN    26
+#define DATA_PIN  25
+#define CS_PIN    27
+
+// Hardware SPI connection
+//MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Arbitrary output pins
+MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
 
 WebServer server(80);  // Server on port 80
 
@@ -48,7 +63,12 @@ void setup() {
     else {
         //if you get here you have connected to the WiFi    
         Serial.println("BBQ connected...yeey :)");
-    }
+    P.begin();
+    P.setIntensity(5); 
+    P.displayClear();    
+  
+
+}
 
 
   
@@ -58,6 +78,10 @@ void setup() {
 
   // Wait for MAX6675 to stabilize
   delay(1000);
+
+
+
+
 }
 
 
@@ -71,8 +95,14 @@ float celsiusToFahrenheit(float celsius) {
   return celsius * 9.0/5.0 + 32.0;
 }
 
- 
-
+ void displayTemperature(float temp) {
+    char tempStr[10];
+//    sprintf(tempStr, "%.1f F", temp);  // Convert float to string with 1 decimal place and append "F" for Fahrenheit
+   sprintf(tempStr, "%.0f F", temp);  // Convert float to string with 1 decimal place and append "F" for Fahrenheit
+    P.displayText(tempStr, PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT);
+    Serial.println(tempStr);
+    P.displayAnimate();
+ }
 
 
 void handleRoot() {
@@ -80,7 +110,10 @@ void handleRoot() {
 
   
   float tempF = celsiusToFahrenheit(thermocouple.readCelsius());
-  
+  displayTemperature(tempF);
+ P.displayAnimate();
+
+
   String html = "<html><head>";
   html += "<meta http-equiv='refresh' content='30'>";
   html += "<style>";
