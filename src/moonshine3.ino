979085807bccf4e23e47309f8714d107a3029529
Grey Lancaster <grey@smallbizserver.com>
2023-10-25 18:40:46 -0400

    Added SPIFFS and FAVICON option

--- src/moonshine3.ino
+++ src/moonshine3.ino
@@ -1,3 +1,11 @@
+// Included an option to post a small image on the website as well as a favicon.jpg
+
+// I found the instructions for SPIFFS @ https://randomnerdtutorials.com/esp32-vs-code-platformio-spiffs/
+
+// I got wledfont.h from a great program @ https://github.com/widapro/wledPixel
+
+//#define USE_SPIFFS  // Uncomment this line if you want to use SPIFFS.
+
 #include <WiFi.h>
 #include <WebServer.h>
 #include <max6675.h>
@@ -5,6 +13,9 @@
 #include <MD_MAX72xx.h>
 #include <MD_Parola.h>
 #include "wledFont.h"
+#include <FS.h>
+#include <SPIFFS.h>
+
 
 void displayTemperature(float temp);
 
@@ -40,11 +51,14 @@ int ip = 0;
 #define BUF_SIZE  75
 char curMessage[BUF_SIZE];
 
-
 WebServer server(80);  // Server on port 80
 char message[50];
 void setup() {
     Serial.begin(115200);
+ if (!SPIFFS.begin()) {
+    Serial.println("An error occurred while mounting SPIFFS");
+ return;
+}
 
     // WiFiManager, Local initialization. Once its business is done, there is no need to keep it around
     WiFiManager wm;
@@ -62,9 +76,8 @@ void setup() {
         
         IPAddress theIP = WiFi.localIP();
         Serial.println(theIP);
-        
- 
-        }
+      
+    }
       currentMillis = millis();
     if (currentMillis - previousMillis > 10000)
     {
@@ -77,18 +90,32 @@ void setup() {
      // Set up first message as the IP address
 
     sprintf(curMessage, "Point your broswer to Http://%03d.%03d.%03d.%03d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
-    // P.displayText("Point your browser to " + String(myIP) , PA_CENTER, 110, 4500, PA_SCROLL_LEFT, PA_NO_EFFECT); 
-    P.displayText(curMessage  , PA_CENTER, 110, 4500, PA_SCROLL_LEFT, PA_NO_EFFECT);   
+    P.displayText(curMessage  , PA_CENTER, 50, 4500, PA_SCROLL_LEFT, PA_NO_EFFECT);   
 
     while (!P.displayAnimate());
     }   
 
     server.on("/", HTTP_GET, handleRoot);
+
+    #ifdef USE_SPIFFS
+        server.on("/greyfox.jpg", HTTP_GET, []() {
+        File file = SPIFFS.open("/greyfox.jpg", "r");
+        server.streamFile(file, "image/jpg");
+        file.close();
+});  
+
+    server.on("/favicon.jpg", HTTP_GET, []() {
+        File file = SPIFFS.open("/favicon.jpg", "r");
+        server.streamFile(file, "image/jpg");
+        file.close();
+});
+#endif
+
     server.begin();
-      // Wait for MAX6675 to stabilize
+
+// Wait for MAX6675 to stabilize
     delay(1000);
     P.setFont(0, wledFont_cyrillic);
-
      
 }
 
@@ -106,12 +133,15 @@ float celsiusToFahrenheit(float celsius) {
     return celsius * 9.0/5.0 + 32.0;
 }
 
+// Declare tempStr as a global variable
+char tempStr[10];
+
+
 void displayTemperature(float temp) {
-    char tempStr[10];
  // alt 0176 gives the degree ° symbol
  //   sprintf(tempStr, "%.1f F", temp);  // Convert float to string with 1 decimal place and append "F" for Fahrenheit
     sprintf(tempStr, "%.0f °F", temp);  // Convert float to string with 0 decimal place and append "F" for Fahrenheit
-     P.displayText(tempStr, PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT); // this display on the MAX7219
+    P.displayText(tempStr, PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT); // this display on the MAX7219
     float tempF = celsiusToFahrenheit(thermocouple.readCelsius());  // Reading temperature here just for the serial display 
     Serial.println(String(tempF));
     P.displayAnimate();
@@ -120,17 +150,26 @@ void displayTemperature(float temp) {
 
 void handleRoot() {
     String html = "<html><head>";
+    #ifdef USE_SPIFFS
+    html += "<link rel=\"icon\" href=\"/favicon.jpg\" type=\"image/jpg\">"; // provides favicon.jpg
+    #endif
+    html += "<meta charset='UTF-8'>";  // Specify UTF-8 encoding
     html += "<meta http-equiv='refresh' content='30'>";
     html += "<style>";
-    //  html += "body { display: flex; height: 100vh;  justify-content: center; font-family: Arial, sans-serif; }";
+    html += "body { display: flex; height: 100vh;  justify-content: center; font-family: Arial, sans-serif; }";
     html += "</style>";
     html += "</head><body>";
     html += "<div>";
     html += "<h1><center>My BBQ</center></h1>";
-    float tempF = celsiusToFahrenheit(thermocouple.readCelsius());  // Reading temperature here just for the webpage display
-    html += "<h1><center>Temperature = " + String(tempF) + " F</center></h1>";
-    html += "<h1><center>at The Grey Fox</center></h1>";                                                            
+    float tempF = celsiusToFahrenheit(thermocouple.readCelsius());
+    //html += "<h1><center>Temperature = " + String(tempF) + " F</center></h1>";  // This shows the Temp on the webpage with 2 decimiles
+    html += "<h1><center>Temperature = " + String(tempStr) + "</center></h1>";  // This shows the Temp on the webpage with no decimles
+    html += "<h1><center>at The Grey Fox</center></h1>";
     html += "</div>";
+    #ifdef USE_SPIFFS
+    html += "<img src='/greyfox.jpg' alt='Grey Fox'width='120' height='80'  >"; //This serves an image named greyfox.jpg from the SPIFFS  
+    #endif    
     html += "</body></html>";
     server.send(200, "text/html", html);
 }
+
